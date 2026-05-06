730e44aa7d882e608a03e5a26e3b39067572cbce
MattBuckley96 <buckleymatt96@gmail.com>
2026-05-06 17:04:19 -0400

    shell update

--- kernel/shell.c
+++ kernel/shell.c
@@ -7,6 +7,9 @@
 #include "io.h"
 
 #define BUF_SIZE 512
+#define MAX_ARGS 8
+
+char arg_buf[BUF_SIZE][MAX_ARGS];
 
 void print_prompt(void)
 {
@@ -57,16 +60,60 @@ void get_line(char* line)
     }
 }
 
-void exec_line(char* line)
+void parse_line(char* line, int* argc, char** argv)
 {
-    if (strcmp(line, "clear") == 0) {
+    memset(arg_buf, 0, sizeof(arg_buf));
+
+    *argc = 0;
+
+    while (*line && *argc < MAX_ARGS) {
+        // skip whitespace
+        while (*line == ' ') {
+            line++;
+        }
+
+        if (*line == '\0') {
+            break;
+        }
+
+        int i = 0;
+        while (*line != ' ' && *line && i < BUF_SIZE - 1) {
+            arg_buf[*argc][i++] = *line++;
+        }
+        arg_buf[*argc][i] = '\0';
+
+        argv[*argc] = arg_buf[*argc];
+        (*argc)++;
+    }
+}
+
+void exec_cmd(int argc, char** argv)
+{
+    if (argc < 1) {
+        return;
+    }
+
+    if (strcmp(argv[0], "clear") == 0) {
         vga_clear();
         return;
     }
 
-    if (strcmp(line, "exit") == 0) {
+    if (strcmp(argv[0], "exit") == 0) {
         // qemu shutdown
         outw(0x604, 0x2000);
+        return;
+    }
+
+    if (strcmp(argv[0], "regs") == 0) {
+        print_regs();
+        return;
+    }
+
+    if (strcmp(argv[0], "echo") == 0) {
+        for (int i = 1; i < argc; i++) {
+            kprintf("%s ", argv[i]);
+        } 
+        kprintf("\n");
         return;
     }
 }
@@ -75,10 +122,15 @@ void shell(void)
 {
     for (;;) {
         char line[BUF_SIZE];
-        memset(line, 0, BUF_SIZE);
+        int argc = 0;
+        char* argv[MAX_ARGS];
+
+        memset(line, 0, sizeof(line));
+        memset(argv, 0, sizeof(argv));
 
         print_prompt();
         get_line(line);
-        exec_line(line);
+        parse_line(line, &argc, argv);
+        exec_cmd(argc, argv);
     }
 }
